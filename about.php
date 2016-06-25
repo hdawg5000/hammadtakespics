c8f02ac2257a92ab50fdee23f0b6085a09f32075
Hammad Naseem <hammadnsm@gmail.com>
2016-06-25 12:18:27 -0500

    Updates to bootstrap implementation

--- about.php
+++ about.php
@@ -6,7 +6,9 @@
         session_start(); 
     }
 
-    print '<div id="about_me"><h1 class="page_title">About me</h1><p>Coming soon...</p><img width="300" height="300" src="images/display.jpg"></div>';
+    print '<div id="about_me"><h1 class="page_title">About me</h1><p>Coming soon...</p><img width="300" height="300" src="images/display.jpg"></div><script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script><script src="js/active.js"></script>';
 
     include('templates/footer.php');
 ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+    <script src="js/active.js"></script>
