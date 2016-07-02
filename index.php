047c2adf231a3a0c6eb69b97d549005dbe0b57d6
Hammad Naseem <hammadnsm@gmail.com>
2016-07-02 18:02:49 -0500

    Updated index carousel with mobile responsiveness

--- index.php
+++ index.php
@@ -12,31 +12,26 @@
             <li data-target="#myCarousel" data-slide-to="2"></li>
             <li data-target="#myCarousel" data-slide-to="3"></li>
         </ol>
-
         <div class="carousel-inner image">
-            <div class="item active">
-                <img src="https://lh3.googleusercontent.com/sfZF7jNtso6KxmSvCEAAKTLdlX7JZdDaDYb1Hok1MZ0dKh99XY7UAsScCaMS3S0Fjg_XHwXgoz2Kw_MfQ9NI3l5mQ2xOwqpFU-B2JXDupCCmbk4utuNycKQELAs2bbMmIX_URTktmDNq459SikdEKyrYHduiuc7xKqYBHRdEdHRrYznC-idOmjWK7OYn__DOETys1LqrgCJOZWPjc16cIsghrPT2QbjhBRoBLw62mFQQULCZIGluweWWulDBmGGA5pCXNfyxM7vlacJgy38-QjiGGkcItqFjfww_vlMrviXkgfJ0BsC-6lREFdeOVuf69WMToePHdv8zVnxotPMn5-jwifGpigMqurBGAiPUO1DpwuLb0vUrEMhIC5i9XoDhlG17kR975PD2dBa1dvJPCkoNxYD_ZWZLt1sYnrPeQGOzGoAmGNiYlREh2-wMhbYfvD85pH34yquv3wqYGPtHQ0GXHQ_Di-iFqCZvbbomOU7eHXqdRUPB5XkUruM7B4DBcfevxb5TrGhaB_TthVU0FHUVJkXMu3_kX9jsnuMDsQ38P179GCYmM2JuaRlJcEbQYZljidBfKi82REgUQvpHD6x4dioC-M-U=w1451-h968-no" alt="St. Peter's Basilica" class="img-responsive" />
+            <div class="item active img1">
                 <div class="carousel-caption">
                     <h2>St. Peter's Basilica</h2>
                     <p>Vatican City</p>
                 </div>
             </div>
-            <div class="item">
-                <img src="https://lh3.googleusercontent.com/6PExeeTNG8VT0rKtm0cIhZD9HXX1q4-njtchNA1AWBrBwoP2ODEpxsTazfCCGWoUpi9it5QRwUWI6U8JhDBh5oKreX0rzygvsNu8p157RYEaIMmg6V87DL-3EpN5iyJUoiR-FC-Tyui8xBOKz0ROob1bvzzxAeNkwQig0x5uQKQs91GeZKDSCb7-UzsAXvhnSQ2XdBFdnhoYqvxFceaIxpcEQAG2bwBXCfd0Pqg0tRg7I622D48JEdmcwRYpSQCFKX2zsVMTpAD31BDEfbxDUpxYHmECtxamsMIr6Tmh_QK9FAXTiEsI5dqpT-urgf3lZtnrAsPOLm8-Ou3m8sILPpGR5KIZCZ9jrhW6yZRT6T3vuRD-P9bCbV5-LjoAHzNWISn3jSNsz6KiCqMPoUii516_1K2sru4ObNcZB5yr3a3Er5YWdjBG23m61HE-FWYcUmHXZes6guCcBPi2FYt1uGCfYyp36z0CU6bHSfT8j0jvZ7HAZi9p2TKGHV0qYeCa_T5qE7a0kGNF8jfDfAgrqpxdpcwRD88a2YPMGYTi-xosEbqdoPO1C7uhJgiXizX5c6jWT5B8kZGJ8a2SnU86TZcyjWULojqo=w1086-h724-no" alt="fountain" class="img-responsive" />
+            <div class="item img2">
                 <div class="carousel-caption">
                     <h2>Dubai</h2>
                     <p>United Arab Emirates</p>
                 </div>
             </div>
-            <div class="item">
-                <img src="https://lh3.googleusercontent.com/fwKeaAULWAoq-MFGpsTGLAw9qIVTK6Apf-wb70WmsAhlnzwnFMox7pXQWaLEqMzZQp49-rO6FGdROKC0e9EFsBi18GgjuBAf0euwRd01sluZOHl11Ya60AZ050hDe1MCaFCWPG8kc2NUGKcW2PCO-aGi0-WIFp8caFRYBl2NACgh49esk6Zq0J3vPGRCmHlvc6vxsrO0KIXLAQLa6AJSFaGOB_lCihNXud1nLtllWTkfwr4zr122Hn22EHeteoYB8fAeACPQqe9zrEjObyD6ysb-OF8JezfJwQxpyr7EBcqBwUbNMV4SdgzB6OFXlUApwaANjmuvJxopCp_Yt7qGfFpXUwDceZ1HrsSgsXykr6cglH9FMsbKev2wioZYdwS0oOS4GKgkusQt0UuN8w0r6q7LjqwyGZZXHy3mZK0212bniBAr5U-n099U3Y_LB6Mf6Qq5LhO1cWXy1LTuRHzY37DhNldgeas3unG-0HW1eXKj7pIXrtWhQjgsjmcT-2t2V6YevIvysmoChEe8MxquO_eGweVkGFl2LuJ3EHVuW195n5kAhigqiQw9Q-He1TcEKbcXQElaP5uuTqx8aZHTzx3UxixpcKK1=w1451-h968-no" alt="boat" class="img-responsive" />
+            <div class="item img3">
                 <div class="carousel-caption">
                     <h2>Venice</h2>
                     <p>Italy</p>
                 </div>
             </div>
-            <div class="item">
-                <img src="https://lh3.googleusercontent.com/-Lo9rtwtDhAwVYTrirpt3rYyxJ4k0m_YJ38b37nD5nV6jkyp-wY5SgkhGTJqgTGlZ27Uc0TKmcPiBqDbD7xIFH7P0rAZkU39yfMiDKb_-crvQvJz__ChyXOAJXao0mFY3x7cD_a8GASXdW6e1nY8pGmRcnlMIWA3U5lAcde6hN_1Rar0L5Xn0WZ_hbAxNftU0b7YGCwLzNJh4n01ArjNC6OfHNBST47FAMaPEl2nqMVlqnbQblk-_-KpuTgHT5tLTjS9w6oAh8xEarddqjMxpgj6g9OnY_Q_OrWIqsP-2H81U3xnN0pjVL2TDdzOibvOK7s-zfxXeS7j6l-9-M9LhWkWve9QJZs8W1tR7c4fRKoyS6wXXamnagWXNTQ4k01VjFCCkPBEtJ9BlujQblJVCAU6JoApd9KdzRnDtuId0SzCg9DkZ8QOEqXITdu6dz-z94zm7Um3-5L7DvvvqhnDqSGbPhLaTd5k-uth2PGKKI4llMp2Y_gHqBair9x3-11QpcWf7XJvUsALrFJC27BsX3dxKLmaa4-Cq8KRIkvzIuNJIJWFhzwUU7h9DWGWapu3a1mjA4A3Hmi_voOxOYCq_KU8z6pRlWfU=w1086-h725-no" alt="Hagia Sophia" class="img-responsive" />
+            <div class="item img4">
                 <div class="carousel-caption">
                     <h2>Hagia Sophia</h2>
                     <p>Turkey</p>
