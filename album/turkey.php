1b40319f547a108b6228963f7a92c3411169d175
Hammad Naseem <hammadnsm@gmail.com>
2016-07-03 22:56:22 -0500

    Updates to lightgallery plugin

--- album/turkey.php
+++ album/turkey.php
@@ -7,21 +7,17 @@
     <link rel="stylesheet" href="../css/style.css" type="text/css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/1.2.21/js/lightgallery.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script>
-
     <h1 id="album_main_title">Turkey</h1>
     <h2><span id="redirect_link"><a href="../albums"><< Go Back</a></span></h2>
     <div class="row">
-        <div class="large-12 columns">
-            <ul class="small-block-grid-2 medium-block-grid-3" id="lightgallery">
-                <li data-src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg"></a>
+        <div class="large-12 columns" id="aniimated-thumbnials">
+            <ul class="small-block-grid-2 medium-block-grid-3 gal" id="lightgallery">
+                <li data-src="https://lh3.googleusercontent.com/L7ZMcpwutHnxjsowVQLQiHx6PvCIySJch9K11M_NnyT2Hyk8m_FjXHWPAokNi6GVfevcB6twKaj4yAp0llSTdQbnYWxS57exjcV7aH4vnPdFH02X6DiSFZB-wOu7AZr6Qez1ITT_yZ35Jbc5p0WTUB1oX405KhAOOV8uOch23z9dpVFih6zdlzcAt47ABwk4Xxc_dQmKCJZA8CFOKC2ZOQjV3M1C3MPKcNModUKoI2zaHOtMG1mXwkhUNSJ9sSWcF9_iSCcTZfOUodjwrFgjTOZx8bWxhF3p9PDxNtk-blxKL7h9nU0WYvSrksmOEWPC41rDHAArjm6aEYqsYEgV6oBY2AtVVOpsNigy8yJm4oPTS6ZJgNabDP2r7_-fG0g1HRX1ZUD3oL5-hPUJNvemnxKPUi-fmMXCR4grjQaGdbjJ1WlE6H7mlMT-ld6su6tJh2Om9tlYCAIewPWsUiaddBp23wrjj_8VdweMy7qVZb7cgnw-YmaAou7UM8WPjXcem1zvPHTRf4lswKnRJi5Hj1wJ0obJmx7a257WBwKNO3M8Qcy8xIzIfThW87C41iFLpy54zHkfl7L1KC215hz8rhFaAOGoc9hI=w727-h1090-no"> <img src="https://lh3.googleusercontent.com/L7ZMcpwutHnxjsowVQLQiHx6PvCIySJch9K11M_NnyT2Hyk8m_FjXHWPAokNi6GVfevcB6twKaj4yAp0llSTdQbnYWxS57exjcV7aH4vnPdFH02X6DiSFZB-wOu7AZr6Qez1ITT_yZ35Jbc5p0WTUB1oX405KhAOOV8uOch23z9dpVFih6zdlzcAt47ABwk4Xxc_dQmKCJZA8CFOKC2ZOQjV3M1C3MPKcNModUKoI2zaHOtMG1mXwkhUNSJ9sSWcF9_iSCcTZfOUodjwrFgjTOZx8bWxhF3p9PDxNtk-blxKL7h9nU0WYvSrksmOEWPC41rDHAArjm6aEYqsYEgV6oBY2AtVVOpsNigy8yJm4oPTS6ZJgNabDP2r7_-fG0g1HRX1ZUD3oL5-hPUJNvemnxKPUi-fmMXCR4grjQaGdbjJ1WlE6H7mlMT-ld6su6tJh2Om9tlYCAIewPWsUiaddBp23wrjj_8VdweMy7qVZb7cgnw-YmaAou7UM8WPjXcem1zvPHTRf4lswKnRJi5Hj1wJ0obJmx7a257WBwKNO3M8Qcy8xIzIfThW87C41iFLpy54zHkfl7L1KC215hz8rhFaAOGoc9hI=w727-h1090-no"></a>
                 </li>
-                <li data-src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/look-out.jpg"> 
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/look-out.jpg"></a> 
-                </li> 
-                <li data-src="img/Fish-th.jpg"> 
-                    <img src="img/Fish-th.jpg"></a> 
-                </li> 
+                <li data-src="https://lh3.googleusercontent.com/XThifC605RdPteugZ_Xptvx4x44ZKF05CBq5_OTvE551lx5OYULSlXXzjpwvBTu8zj7W5a3-FAa3uSkaBkndwhxjCj61j39lEvqHWeum-tnX_URFwU8wAi78zukWvyVQEPXakz-yfsCgpBBTfQyy2wyq-S6H3jdspoC2EmScKHBXoV7SJzK5o8d6b8u9pzLS9X4B-QJuPTzZm054Ck67G38WBbx29Jq_Ooh8fit0WKAUGqiIvdpm4mX4rOvoNfEQbHiGeqVqW-2xJHI-JeHaKuFqj-0kHaO0_dUVBDFmHbtWCbNEazRjzKo5quGDlheIDceN0YNd1STF4TDHaoYN0nPUxdIcS2DI-P29ENK7r8BgfflVaxHTkWB5D7bLa5lyOE_a4MkX7AeQwc7qP-xXqajuIJ7D0WpRpWvOtvCfDhELXr-sf6ESGiIqmOXneAk2MIOBFQdejMkOW3JRvIRVAw5WXuVDMmVulsK_eYT3Wi_kuX5ltGGI7QEZS1JS773GvfQjeTWki6EFm_OHZFQLmBIMvj54b6i4nrztJe6q3Po7-Wr045GJkRRIGrTjGa4DvIWJ6j33F7XGP-hrfmwj26K9_LaoWNOI=w1400-h934-no"> <img src="https://lh3.googleusercontent.com/XThifC605RdPteugZ_Xptvx4x44ZKF05CBq5_OTvE551lx5OYULSlXXzjpwvBTu8zj7W5a3-FAa3uSkaBkndwhxjCj61j39lEvqHWeum-tnX_URFwU8wAi78zukWvyVQEPXakz-yfsCgpBBTfQyy2wyq-S6H3jdspoC2EmScKHBXoV7SJzK5o8d6b8u9pzLS9X4B-QJuPTzZm054Ck67G38WBbx29Jq_Ooh8fit0WKAUGqiIvdpm4mX4rOvoNfEQbHiGeqVqW-2xJHI-JeHaKuFqj-0kHaO0_dUVBDFmHbtWCbNEazRjzKo5quGDlheIDceN0YNd1STF4TDHaoYN0nPUxdIcS2DI-P29ENK7r8BgfflVaxHTkWB5D7bLa5lyOE_a4MkX7AeQwc7qP-xXqajuIJ7D0WpRpWvOtvCfDhELXr-sf6ESGiIqmOXneAk2MIOBFQdejMkOW3JRvIRVAw5WXuVDMmVulsK_eYT3Wi_kuX5ltGGI7QEZS1JS773GvfQjeTWki6EFm_OHZFQLmBIMvj54b6i4nrztJe6q3Po7-Wr045GJkRRIGrTjGa4DvIWJ6j33F7XGP-hrfmwj26K9_LaoWNOI=w1400-h934-no"></a>
+                </li>
+                <li data-src="https://lh3.googleusercontent.com/XThifC605RdPteugZ_Xptvx4x44ZKF05CBq5_OTvE551lx5OYULSlXXzjpwvBTu8zj7W5a3-FAa3uSkaBkndwhxjCj61j39lEvqHWeum-tnX_URFwU8wAi78zukWvyVQEPXakz-yfsCgpBBTfQyy2wyq-S6H3jdspoC2EmScKHBXoV7SJzK5o8d6b8u9pzLS9X4B-QJuPTzZm054Ck67G38WBbx29Jq_Ooh8fit0WKAUGqiIvdpm4mX4rOvoNfEQbHiGeqVqW-2xJHI-JeHaKuFqj-0kHaO0_dUVBDFmHbtWCbNEazRjzKo5quGDlheIDceN0YNd1STF4TDHaoYN0nPUxdIcS2DI-P29ENK7r8BgfflVaxHTkWB5D7bLa5lyOE_a4MkX7AeQwc7qP-xXqajuIJ7D0WpRpWvOtvCfDhELXr-sf6ESGiIqmOXneAk2MIOBFQdejMkOW3JRvIRVAw5WXuVDMmVulsK_eYT3Wi_kuX5ltGGI7QEZS1JS773GvfQjeTWki6EFm_OHZFQLmBIMvj54b6i4nrztJe6q3Po7-Wr045GJkRRIGrTjGa4DvIWJ6j33F7XGP-hrfmwj26K9_LaoWNOI=w1400-h934-no"> <img src="https://lh3.googleusercontent.com/XThifC605RdPteugZ_Xptvx4x44ZKF05CBq5_OTvE551lx5OYULSlXXzjpwvBTu8zj7W5a3-FAa3uSkaBkndwhxjCj61j39lEvqHWeum-tnX_URFwU8wAi78zukWvyVQEPXakz-yfsCgpBBTfQyy2wyq-S6H3jdspoC2EmScKHBXoV7SJzK5o8d6b8u9pzLS9X4B-QJuPTzZm054Ck67G38WBbx29Jq_Ooh8fit0WKAUGqiIvdpm4mX4rOvoNfEQbHiGeqVqW-2xJHI-JeHaKuFqj-0kHaO0_dUVBDFmHbtWCbNEazRjzKo5quGDlheIDceN0YNd1STF4TDHaoYN0nPUxdIcS2DI-P29ENK7r8BgfflVaxHTkWB5D7bLa5lyOE_a4MkX7AeQwc7qP-xXqajuIJ7D0WpRpWvOtvCfDhELXr-sf6ESGiIqmOXneAk2MIOBFQdejMkOW3JRvIRVAw5WXuVDMmVulsK_eYT3Wi_kuX5ltGGI7QEZS1JS773GvfQjeTWki6EFm_OHZFQLmBIMvj54b6i4nrztJe6q3Po7-Wr045GJkRRIGrTjGa4DvIWJ6j33F7XGP-hrfmwj26K9_LaoWNOI=w1400-h934-no"></a>
+                </li>
             </ul>
         </div>
     </div>
@@ -41,6 +37,9 @@
 -->
     <script type="text/javascript">
         $('#lightgallery').lightGallery();
+        $('#aniimated-thumbnials').lightGallery({
+            thumbnail: true
+        })
     </script>
     <!--
 <div class="grid">
--- templates/footer.php
+++ templates/footer.php
@@ -1,26 +1,20 @@
+</div>
 <!-- Start of footer -->
-<div class="navbar navbar-default navbar-bottom" role="navigation">
+<div class="navbar navbar-default navbar-bottom footer" role="navigation">
     <div class="container">
         <div class="navbar-text pull-right">
             <p>&copy
-                <?php print date("Y"); ?> Hammad Takes Pics</p>
-            <a href="https://www.facebook.com/hammadtakespics/" target="_blank"><i class="foot fa fa-facebook-square fa-3x"></i></a>
-            <a href="#" target="_blank"><i class="foot fa fa-google-plus-official fa-3x"></i></a>
-            <a href="https://www.instagram.com/hammadtakespics/" target="_blank"><i class="foot fa fa-instagram fa-3x"></i></a>
-            <a href="#" target="_blank"><i class="foot fa fa-twitter fa-3x"></i></a>
-            <a href="#" target="_blank"><i class="foot fa fa-youtube-play fa-3x"></i></a>
+                <?php print date("Y"); ?> Hammad Takes Pics</p> <a href="https://www.facebook.com/hammadtakespics/" target="_blank"><i class="foot fa fa-facebook-square fa-3x"></i></a> <a href="#" target="_blank"><i class="foot fa fa-google-plus-official fa-3x"></i></a> <a href="https://www.instagram.com/hammadtakespics/" target="_blank"><i class="foot fa fa-instagram fa-3x"></i></a> <a href="#" target="_blank"><i class="foot fa fa-twitter fa-3x"></i></a> <a href="#" target="_blank"><i class="foot fa fa-youtube-play fa-3x"></i></a>
             <!--
                     <a href="https://www.instagram.com/hammadtakespics/" target="_blank"><img src="images/instagram_2016_icon.png"/></a>
                 </li>
                 <li>
                     <a href="https://www.facebook.com/hammadtakespics/" target="_blank"><img src="images/square-facebook-128.png"/></a>
                 </li>
--->
-            </ul>
+--></ul>
         </div>
     </div>
 </div>
-
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
