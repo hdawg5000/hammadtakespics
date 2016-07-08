01aa55030d6767193d99c01c48692fab23f4a281
Hammad Naseem <hammadnsm@gmail.com>
2016-07-07 20:15:21 -0500

    Fixed src files for lightgallery.js

--- templates/header.php
+++ templates/header.php
@@ -14,15 +14,15 @@
         <link href="css/bootstrap.css" rel="stylesheet">
         <link href="css/style.css" rel="stylesheet">
         <!-- LightGallery CSS -->
-        <link type="text/css" rel="stylesheet" href="../css/lightgallery.css" />
+        <link type="text/css" rel="stylesheet" href="css/lightgallery.css" />
         <!-- Font Awesome -->
         <script src="https://use.fontawesome.com/b9dd0c1c41.js"></script>
         <!-- jQuery -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
         <!-- LightGallery JS -->
-        <script src="../js/lightgallery.js"></script>
-        <script src="../js/lg-thumbnail.js"></script>
-        <script src="../js/lg-fullscreen.js"></script>
+        <script src="js/lightgallery.js"></script>
+        <script src="js/lg-thumbnail.js"></script>
+        <script src="js/lg-fullscreen.js"></script>
         <!-- A jQuery plugin that adds cross-browser mouse wheel support. (Optional) -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.13/jquery.mousewheel.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script>
