ab3d8e9c782187faea411a4ae8125bed4ab3dc95
Hammad Naseem <hammadnsm@gmail.com>
2016-07-08 20:57:07 -0500

    Added href in footer for YouTube channel

--- album/templates/footer.php
+++ album/templates/footer.php
@@ -2,24 +2,17 @@
 <div class="navbar navbar-default navbar-bottom" role="navigation">
     <div class="container">
         <div class="navbar-text pull-right">
-            <p>&copy 2016 Hammad Takes Pics</p>
-            <a href="https://www.facebook.com/hammadtakespics/" target="_blank"><i class="foot fa fa-facebook-square fa-3x"></i></a>
-            <a href="#" target="_blank"><i class="foot fa fa-google-plus-official fa-3x"></i></a>
-            <a href="https://www.instagram.com/hammadtakespics/" target="_blank"><i class="foot fa fa-instagram fa-3x"></i></a>
-            <a href="#" target="_blank"><i class="foot fa fa-twitter fa-3x"></i></a>
-            <a href="#" target="_blank"><i class="foot fa fa-youtube-play fa-3x"></i></a>
+            <p>&copy 2016 Hammad Takes Pics</p> <a href="https://www.facebook.com/hammadtakespics/" target="_blank"><i class="foot fa fa-facebook-square fa-3x"></i></a> <a href="#" target="_blank"><i class="foot fa fa-google-plus-official fa-3x"></i></a> <a href="https://www.instagram.com/hammadtakespics/" target="_blank"><i class="foot fa fa-instagram fa-3x"></i></a> <a href="#" target="_blank"><i class="foot fa fa-twitter fa-3x"></i></a> <a href="https://www.youtube.com/channel/UCVQwzp1Qpr2FONc5DJS2agg" target="_blank"><i class="foot fa fa-youtube-play fa-3x"></i></a>
             <!--
                     <a href="https://www.instagram.com/hammadtakespics/" target="_blank"><img src="images/instagram_2016_icon.png"/></a>
                 </li>
                 <li>
                     <a href="https://www.facebook.com/hammadtakespics/" target="_blank"><img src="images/square-facebook-128.png"/></a>
                 </li>
--->
-            </ul>
+--></ul>
         </div>
     </div>
 </div>
-
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
