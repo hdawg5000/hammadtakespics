e483695feedd2bc87447992b73d531b7069ad16c
Hammad Naseem <hammadnsm@gmail.com>
2016-07-10 07:45:32 -0500

    Added Italy and Istanbul videos. Finalized styling

--- videos.php
+++ videos.php
@@ -5,16 +5,21 @@
 ?>
     <link rel="stylesheet" href="album/css/style.css" type="text/css" />
     <link rel="stylesheet" href="album/css/bootstrap.css" type="text/css" />
-    <div class="wrapper">
-        <h1 id="album_main">Videos</h1>
-        <h2>Dubai | January 2016</h2>
+    <div class="videos_main_content">
+        <h1 class="videos_main_title">Videos</h1>
+        <h2 id="yt_video_title">Dubai | January 2016</h2>
         <div class="yt-video">
             <iframe width="640" height="480" src="https://www.youtube.com/embed/RQtjDbXMQN4" frameborder="0" allowfullscreen></iframe>
         </div>
-        <div class="push"> </div>
+        <h2 id="yt_video_title">Italy | January 2016</h2>
+        <div class="yt-video">
+            <iframe width="640" height="480" src="https://www.youtube.com/embed/IyaGh9zNmmY" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <h2 id="yt_video_title">Istanbul | December 2015</h2>
+        <div class="yt-video">
+            <iframe width="640" height="480" src="https://www.youtube.com/embed/aEGSfaIaKQQ" frameborder="0" allowfullscreen></iframe>
+        </div>
     </div>
-    <div class="footer ">
-        <?php
+    <?php
     include('templates/footer.php');
 ?>
-    </div>
