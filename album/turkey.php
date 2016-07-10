8fb7c0c2d234a7b6bb4d020b4ceed82fa26f2f00
Hammad Naseem <hammadnsm@gmail.com>
2016-07-09 20:41:41 -0500

    Updated styling in gallery pages

--- album/turkey.php
+++ album/turkey.php
@@ -5,13 +5,16 @@
 ?>
     <link rel="stylesheet" href="../css/bootstrap.css" type="text/css" />
     <link rel="stylesheet" href="../css/style.css" type="text/css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/1.2.21/js/lg-thumbnail.min.js"></script>
+    <script src="
+https://cdnjs.cloudflare.com/ajax/libs/lightgallery/1.2.21/js/lg-fullscreen.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/1.2.21/js/lightgallery.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <h1 id="album_main_title">Turkey</h1>
     <h2><span id="redirect_link"><a href="../albums"><< Go Back</a></span></h2>
     <div class="row">
-        <div class="large-12 columns">
-            <ul class="small-block-grid-2 medium-block-grid-3" id="lightgallery">
+        <div class="large-12 columns" id="aniimated-thumbnials">
+            <ul class="small-block-grid-2 medium-block-grid-3 piks" id="lightgallery">
                 <li data-src="https://lh3.googleusercontent.com/v7KPJgiCZwnKcpMqp8vB_symQ5DMrvdFD5fNF3cweryAl1QiOhT0-vc7Qw6uh-aPhj_F9puj8CLdX3EDir5fAhM98AyPTwKggxW-QGwVmlx5lE9ll7PEFLqlcJKion2z-yYu8XYd5TXdQp1TWaCo2QdeAfcz2yn0gwdMLf-c14Kjm4OZ_aLAN0ydTJD3PlH5yRh0e4CCbFnmYs6Tf4-AluWVpJJGx_kBjisSwlwRjNgSnBD7OYIdH1ByUug7q4aecSq8_cluSkeupIau2NEmlsmqpAxL1QOIim6jQmQv05WuL0i0BRxuUsmk8prCx461R3YukwKF0EeuHhEWI0ppIxUT0PGAHEhkKctuuTpRsWVnG__6w99UpvKuNG9LTpJFEq4ILuTmYm3OcVjdPRZzVfs3ABtkU4h0gF6V2ilH7Jo242UB2np7dPOFx2MnOtrhcjFs3CZjGG17t-aM7i_hBGFJIhcCVDuXhPhoIIiBLaEfSP1gTQSseBS3SNBSXO0lR0EHvOEqujuh2Skvk2MaLhNSdqMRB_3rhdpy63Dn48i3DXoFqbtvGUqBN1pX6GsHdtTrkhJEeZQfpQEyn9jX4_ZimXW-D6ck=w727-h1090-no"> <img src="https://lh3.googleusercontent.com/v7KPJgiCZwnKcpMqp8vB_symQ5DMrvdFD5fNF3cweryAl1QiOhT0-vc7Qw6uh-aPhj_F9puj8CLdX3EDir5fAhM98AyPTwKggxW-QGwVmlx5lE9ll7PEFLqlcJKion2z-yYu8XYd5TXdQp1TWaCo2QdeAfcz2yn0gwdMLf-c14Kjm4OZ_aLAN0ydTJD3PlH5yRh0e4CCbFnmYs6Tf4-AluWVpJJGx_kBjisSwlwRjNgSnBD7OYIdH1ByUug7q4aecSq8_cluSkeupIau2NEmlsmqpAxL1QOIim6jQmQv05WuL0i0BRxuUsmk8prCx461R3YukwKF0EeuHhEWI0ppIxUT0PGAHEhkKctuuTpRsWVnG__6w99UpvKuNG9LTpJFEq4ILuTmYm3OcVjdPRZzVfs3ABtkU4h0gF6V2ilH7Jo242UB2np7dPOFx2MnOtrhcjFs3CZjGG17t-aM7i_hBGFJIhcCVDuXhPhoIIiBLaEfSP1gTQSseBS3SNBSXO0lR0EHvOEqujuh2Skvk2MaLhNSdqMRB_3rhdpy63Dn48i3DXoFqbtvGUqBN1pX6GsHdtTrkhJEeZQfpQEyn9jX4_ZimXW-D6ck=w727-h1090-no"></a>
                 </li>
                 <li data-src="https://lh3.googleusercontent.com/oHgxBMgc8hYqNqfZYk_BN-mMOW7IW3d08WbOlguEC_-rfr3Zc0jWsyCcx6cnm5uliBEtgB26NHB7JePmgy1Y2zFPzS2FvKhL3bSPDJLswGdwlLnl2M-FzBByEHmm39KMVOWEVWNAYuw3__N0Wxnt9j1uOZU50wp4q_S69xPWNLR8SXrUNzfydOMRIoU2gpBCRKsu2fF1bniU7mLaiR7gJkHqQH-rLLIOfzm1SFGYdv6rq_0I_GZBX8bvLxLJR_Vr1a9aTsUle4ZPfxY_ssFoHFA_ypp1s7Q8t9Gp9NZhuzBPbaWXsTMJXzxfILmavJJP3Xj3ntvBptChe6jn8uOdnl4NQwWo0vo8fBl-Zgu9FbUxEXodmGKVc1kivtwx2CPlp0fBNJHXFA96cYcPnCSuKSLQhTTxPnS5QQSUZReqrdJfBOxihXcRT5yW40r-Q8YVwtt6nXItWFbGMnknK-wFViqVonOIiwYs__ErL2ftmqfKHKsRuYTpx1-lE5kk6dFKEN_CF1shIigpAYt3M_yg_E10sWhYQ_Gh8pc4Khzwd2ZGWD8qISSCQAbpfoga4gFLXfQULec-BVtKsUtVc4I0H6Iq9H_qZ2dh=w1247-h832-no"> <img src="https://lh3.googleusercontent.com/oHgxBMgc8hYqNqfZYk_BN-mMOW7IW3d08WbOlguEC_-rfr3Zc0jWsyCcx6cnm5uliBEtgB26NHB7JePmgy1Y2zFPzS2FvKhL3bSPDJLswGdwlLnl2M-FzBByEHmm39KMVOWEVWNAYuw3__N0Wxnt9j1uOZU50wp4q_S69xPWNLR8SXrUNzfydOMRIoU2gpBCRKsu2fF1bniU7mLaiR7gJkHqQH-rLLIOfzm1SFGYdv6rq_0I_GZBX8bvLxLJR_Vr1a9aTsUle4ZPfxY_ssFoHFA_ypp1s7Q8t9Gp9NZhuzBPbaWXsTMJXzxfILmavJJP3Xj3ntvBptChe6jn8uOdnl4NQwWo0vo8fBl-Zgu9FbUxEXodmGKVc1kivtwx2CPlp0fBNJHXFA96cYcPnCSuKSLQhTTxPnS5QQSUZReqrdJfBOxihXcRT5yW40r-Q8YVwtt6nXItWFbGMnknK-wFViqVonOIiwYs__ErL2ftmqfKHKsRuYTpx1-lE5kk6dFKEN_CF1shIigpAYt3M_yg_E10sWhYQ_Gh8pc4Khzwd2ZGWD8qISSCQAbpfoga4gFLXfQULec-BVtKsUtVc4I0H6Iq9H_qZ2dh=w1247-h832-no"></a>
@@ -23,6 +26,9 @@
     </div>
     <script type="text/javascript">
         $('#lightgallery').lightGallery();
+        $('#aniimated-thumbnials').lightGallery({
+            thumbnail: true
+        })
     </script> <a href="https://hammadtakespics.com/album/turkey#">Go to Top</a>
     <?php
         include('templates/footer.php');
