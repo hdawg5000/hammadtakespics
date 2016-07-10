2454d6f39f2b61d91d33b74fb167224794e81f67
Hammad Naseem <hammadnsm@gmail.com>
2016-07-10 14:59:47 -0500

    Added dubai.php and updated albums.php with link to dubai.php

--- albums.php
+++ albums.php
@@ -21,6 +21,12 @@
             </a>
             </br>
             </br>
+            <h2>Dubai</h2></br>
+            <a href="album/dubai">
+                <li><img src="https://lh3.googleusercontent.com/UzN_idheVlT5FL2tJPNIUT1lfRhqJHjoymiL-BaBwTweNlHTKMQtFAyevn6P8g6HhVudgelXoz5qlRJVY5EVKgmVrfSpp0b6tYpCRejv4AJxWWWmlTxr-E7Oi6q30-rsPVvAfTQJ9gEfdkdaOlq0vO2nBsHEyh04lCClPTik5wSwR52HZxCB7m9xcvaST6VQiWsktjwka23ji2lrZIsUql2a6NP0fTbua9g1SRQZbJg3pWiTVL2V-_KGJQNh2OoVNzJApc1SapYPtOKwD1EtTtIN_2t1xaDBHla0IHMmK-kx4fmCE4CuWPf5DxGmJRMSqRguVa2sFNBni07mgAonE6GeObOlsYtJdKhHF2svgoOvlecx7YErvx3WSAeCgQru2wtIAZdcdjZG1KXfXn4GeK3EFqH9KfFyKSKVdwGWdgvg0Xr8pVldOVzgknXvK8yejX9OHZhXFKU-ooYP9KRFKaAmJBoNzA4_wb_dl9_5SHUu9l29txDmsEwcGvdhEpISteteqiT4tQ9c3MY4F1Fix3KfUUbZQqvGH4kH_D_tsehlkNdCL8o28GxXST6EKjmhB4HDHP0KM83RVOof9H5pz9MJqM_NzWRu=w1250-h834-no"></li>
+            </a>
+            </br>
+            </br>
         </ul>
     </div>'; 
     include('templates/footer.php'); 
