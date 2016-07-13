c12495a39f1651b17d1346ef39535c88cbc7f62f
Hammad Naseem <hammadnsm@gmail.com>
2016-07-13 16:37:22 -0500

    updates to footer

--- album/templates/footer.php
+++ album/templates/footer.php
@@ -1,16 +1,8 @@
 <!-- Start of footer -->
 <div class="navbar navbar-default navbar-bottom" role="navigation">
     <div class="container">
-        <div class="adsoptimal-slot" style="width: 300px; height: 250px;"></div>
         <div class="navbar-text pull-right">
             <p>&copy 2016 Hammad Takes Pics</p> <a href="https://www.facebook.com/hammadtakespics/" target="_blank"><i class="foot fa fa-facebook-square fa-3x"></i></a> <a href="#" target="_blank"><i class="foot fa fa-google-plus-official fa-3x"></i></a> <a href="https://www.instagram.com/hammadtakespics/" target="_blank"><i class="foot fa fa-instagram fa-3x"></i></a> <a href="https://twitter.com/hammadtakespics" target="_blank"><i class="foot fa fa-twitter fa-3x"></i></a> <a href="https://www.youtube.com/channel/UCVQwzp1Qpr2FONc5DJS2agg" target="_blank"><i class="foot fa fa-youtube-play fa-3x"></i></a>
-            <!--
-                    <a href="https://www.instagram.com/hammadtakespics/" target="_blank"><img src="images/instagram_2016_icon.png"/></a>
-                </li>
-                <li>
-                    <a href="https://www.facebook.com/hammadtakespics/" target="_blank"><img src="images/square-facebook-128.png"/></a>
-                </li>
---></ul>
         </div>
     </div>
 </div>
