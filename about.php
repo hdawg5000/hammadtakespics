5c7d55dcc8b0db3088e60920990cbd786c1ff216
Hammad Naseem <hammadnsm@gmail.com>
2016-07-13 09:45:48 -0500

    Updated about.php code

--- about.php
+++ about.php
@@ -6,10 +6,18 @@
         session_start(); 
     }
 
-    print '<div id="about_me"><h1 class="page_title">About me</h1><p>Coming soon...</p><img width="300" height="300" src="images/display.jpg"></div><script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script><script src="js/active.js"></script>';
+?>
 
+    <div id="about_me">
+        <h1 class="page_title">About me</h1>
+        <p>Coming soon...</p>
+        <img width="300" height="300" src="img/display.jpg">
+    </div>
+
+<?php
     include('templates/footer.php');
 ?>
     <script src="js/active.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script><script src="js/active.js"></script>
 <link rel="stylesheet" href="album/css/bootstrap.css" type="text/css" />
 <link rel="stylesheet" href="album/css/style.css" type="text/css" />
